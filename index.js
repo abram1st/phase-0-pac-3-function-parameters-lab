de44ebdbd9e22171b7ce09e98acf469cde5ca167
abram1st <abram.luntungan97@gmail.com>
2021-04-15 03:09:52 -0600

    completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+function introduction (Name=Josh) {
+    return "Hi, my name is Josh."
+}
+function introductionWithLanguage(Name=Josh, Language=Emberjs) {
+     return "Hi, my name is Josh and I am learning to program in Ember.js"
+}
+function introductionWithLanguageOptional(Name=Gracie, Language=Python) {
+    console.log ("Hi, my name is Gracie and I am learning to program in Python")
+}
